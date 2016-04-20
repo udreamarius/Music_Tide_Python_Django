2eca82faf1b763f422032aa1156af47a06f0e27e
udreamarius <marius_razvan.udrea@aut.pub.ro>
2016-04-20 05:33:05 -0700

    added modules

---
+++ tide/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Albums',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('artist', models.CharField(max_length=250)),
+                ('album_title', models.CharField(max_length=500)),
+                ('genre', models.CharField(max_length=100)),
+                ('album_logo', models.CharField(max_length=1000)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Song',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('file_type', models.CharField(max_length=10)),
+                ('song_title', models.CharField(max_length=20)),
+                ('album', models.ForeignKey(to='tide.Albums')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
